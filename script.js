0ae96676586e9cd252352b72c3d9b13b273b2764
Eldorbekkh <erxdealer0990@gmail.com>
2022-12-07 01:48:34 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let hamburger = document.querySelector(".hamb");
+let navlist = document.querySelector(".nav-list");
+document.querySelector(".nav-list li");
+hamburger.addEventListener("click", function (){
+    this.classList.toggle("click");
+    navlist.classList.toggle("open");
+});
